fdee12d18d29112f7b12bf452333437f501e01f9
清水佳祐 <shimizukeisuke@shimizukeisuke-no-MacBook-Air.local>
2016-05-20 18:05:49 +0900

    訂正版

--- GameFieldPanel.java
+++ GameFieldPanel.java
@@ -8,6 +8,8 @@ class GameFieldPanel extends JPanel {
     }
     protected void paintComponent(Graphics g) {
 	super.paintComponent(g);
+	Graphics2D g2d = (Graphics2D)g;
+	Graphics2D g2 = (Graphics2D)g;
 	g.setColor(Color.BLACK);
 	g.drawRect(0,0,384,384);
 
@@ -17,5 +19,15 @@ class GameFieldPanel extends JPanel {
 	for(int y = 0; y < 384; y+= 64) {
 	    g.drawLine(0,y,384,y);
 	}
+
+	Font font = new Font("Arial",Font.BOLD,30);
+	g2.setFont(font);
+
+	for(int y = 0; y < 384; y+= 64) {
+	    for(int x = 0; x < 384; x+= 64) {
+		g2d.drawString("1", x + 23, y + 40);
+	    }
+	}
+
     }
 }
