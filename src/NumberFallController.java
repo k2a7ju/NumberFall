7b8875a676bcb939b47ce09bcf46198cd772c8e5
kaju2759hiro <kajuringo72@gmail.com>
2016-05-25 14:45:17 +0900

    動くようになった ver.2

--- src/NumberFallController.java
+++ src/NumberFallController.java
@@ -12,16 +12,10 @@ public class NumberFallController implements MouseListener{
 
     private int x = 0;
     private int y = 0;
-<<<<<<< HEAD
     private int xa = 64,ya = 64;
     private int box1 = 0,box2 = 0;
     private int clickCount = 0;
     private Point pointxy = new Point(0,0);
-=======
-    private int xa=64,ya=64;
-    private int box1=0,box2=0;
-    private Point pointxy=new Point(0,0);
->>>>>>> 0f62678ec38b892c7daaf6e0d3065a7fdd95a6b9
 
     public int checkPair(int box1,int box2){
 	for(int i = 0; i < 6; i++){
@@ -47,20 +41,10 @@ public class NumberFallController implements MouseListener{
     public int getbox2(){
 	return box2;
     }
-    public Point getPointxy(){
-	return pointxy;
-    }
-    public int getbox1(){
-	return box1;
-    }
-    public int getbox2(){
-	return box2;
-    }
 	
     public void mouseClicked(MouseEvent e){
       x = e.getX();
       y = e.getY();
-<<<<<<< HEAD
       pointxy = e.getPoint();
       int boxNumber = 1;
       //System.out.println("X = "+x+"Y = "+y);
@@ -81,22 +65,6 @@ public class NumberFallController implements MouseListener{
 		      }
 		      System.out.println("box2= "+box2);
 		      this.clickCount = 0;
-=======
-      pointxy=e.getPoint();
-      int boxnumber=1;
-      System.out.println("X = "+x+"Y = "+y);
-      for(int j=0;j<384;j=j+ya){
-	  for(int i=0;i<384;i=i+xa){
-	      if((x>=i&&x<i+xa)&&(y>=j&&y<j+ya)){
-		  if(box1==0&&box2==0){
-		      box1=boxnumber;
-		      System.out.println("box1="+box1);
-		  }else if(box1!=0&&box2==0){
-		      box2=boxnumber;
-		      System.out.println("box2="+box2);
-		      
-		  }else{
->>>>>>> 0f62678ec38b892c7daaf6e0d3065a7fdd95a6b9
 		  }
 	      }
 	      boxNumber += 1;
